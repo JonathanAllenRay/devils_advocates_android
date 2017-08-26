0575664ebc3a3a3cc41e2126cfc494eef6002bc9
JonathanAllenRay <jonathanallenray1@gmail.com>
2017-08-26 02:15:42 -0500

    Just fuckin' work pos

---
+++ app/src/main/java/jonathanray/classyconversations/MainActivity.java
@@ -0,0 +1,22 @@
+package jonathanray.classyconversations;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void goToInstructions(View view) {
+        Intent intent = new Intent(this, DisplayInstructions.class);
+        startActivity(intent);
+    }
+}
