72a4e02ec6a699e218614cf50f007ce139c11d16
JonathanAllenRay <jonathanallenray1@gmail.com>
2017-09-01 16:31:57 -0500

    Setup pre-round screen, added preference reading. Errors: unconstrained placeholders

--- app/src/main/java/jonathanray/classyconversations/GameplayMain.java
+++ app/src/main/java/jonathanray/classyconversations/GameplayMain.java
@@ -1,8 +1,12 @@
 package jonathanray.classyconversations;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,11 +18,31 @@ public class GameplayMain extends AppCompatActivity {
 
     private PlayerList playerList;
 
+    boolean randomPlayers;
+    boolean randomJudge;
+    boolean loserStays;
+    boolean juryMode;
+    int timeLimit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_gameplay_main);
         Bundle b = getIntent().getExtras();
         playerList = b.getParcelable("players");
+        TextView textView = (TextView) findViewById(R.id.titleView);
+        textView.setText("Round " + playerList.getRoundNum());
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        randomPlayers = prefs.getBoolean("random_players", true);
+        randomJudge = prefs.getBoolean("random_judge", true);
+        loserStays = prefs.getBoolean("loser_stays", true);
+        juryMode = prefs.getBoolean("jury_mode", true);
+        timeLimit = Integer.valueOf(prefs.getString("round_time", "20"));
+        if (playerList.size() < 3) {
+            textView.setText("Round " + playerList.getRoundNum() +": Insufficient players, 3 or more" +
+                    " required to play. Return to player screen and add more players.");
+        }
     }
 }
--- app/src/main/java/jonathanray/classyconversations/PlayerList.java
+++ app/src/main/java/jonathanray/classyconversations/PlayerList.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 public class PlayerList extends ArrayList<Player> implements Parcelable {
 
     int cursor;
+    int roundNum;
 
     public PlayerList(){
+        roundNum = 1;
         cursor = 0;
     }
 
@@ -19,6 +21,7 @@ public class PlayerList extends ArrayList<Player> implements Parcelable {
     public PlayerList(Parcel in){
         this.clear();
         int size = in.readInt();
+        this.roundNum = in.readInt();
         this.cursor = in.readInt();
 
         for (int i = 0; i < size; i++) {
@@ -32,6 +35,7 @@ public class PlayerList extends ArrayList<Player> implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         int size = this.size();
         dest.writeInt(size);
+        dest.writeInt(this.roundNum);
         dest.writeInt(this.cursor);
         for (int i = 0; i < size; i++) {
             Player player = this.get(i);
@@ -40,6 +44,29 @@ public class PlayerList extends ArrayList<Player> implements Parcelable {
             dest.writeInt(player.getRoundsPlayed());
         }
 
+    }
+
+    public int getCursor() {
+        return cursor;
+    }
+
+    public int getRoundNum() {
+        return roundNum;
+    }
+
+    public void setCursor(int n) {
+        cursor = n;
+    }
+
+    public void incrementRound() {
+        roundNum++;
+    }
+
+    public void incrementCursor() {
+        cursor++;
+        if (cursor >= this.size()) {
+            cursor = 0;
+        }
     }
 
     public int describeContents() {
